6b80fe0b327643369a701c53726043e7386a05d2
Arseni Luchinin <143221669+ArseniLuchinin@users.noreply.github.com>
2024-05-18 18:25:08 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot
+from scipy import linalg
+
+a = [1, 2, 3, 10]
+b = [12, 14, 45, 92]
+
+pyplot.plot(a, b, color='red', marker='o', linestyle='--')
+pyplot.bar(a, b, color='blue')
+pyplot.show()
+
+print("hello word")
+
